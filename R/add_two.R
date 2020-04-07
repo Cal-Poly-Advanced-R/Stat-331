86dddf9dd9f8e93e30705ad8c2bf349247dcf33a
Kelly Bodwin <kbodwin@calpoly.edu>
2020-04-07 11:49:23 -0700

    public 331
    sharing materials

---
+++ R/add_two.R
@@ -0,0 +1,12 @@
+#' for to add stuff
+#' 
+#' @param x a stuff
+#' 
+#' @return A number
+#' 
+
+add_two <- function(x) {
+  
+  x + 2
+  
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# STAT-331
+ Files for Stat 331: Statistical Computing with R
